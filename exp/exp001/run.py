089f92841308b07c89a5a8a99b14409f65474533
fkubota <rkkubotay@gmail.com>
2020-12-13 19:11:41 +0900

    [Update]exp001

--- exp/exp001/run.py
+++ exp/exp001/run.py
@@ -1,17 +1,30 @@
+import os
 import yaml
-import glob
+from glob import glob
 from lib import util
 from loguru import logger
 
 
+def init_exp(config):
+    # output dirの作成
+    _dir = os.path.dirname(os.path.abspath(__file__))
+    exp_name = _dir.split('/')[-1]
+    dir_exp = f'{config["globals"]["dir_save"]}{exp_name}'
+    if not os.path.exists(dir_exp):
+        os.makedirs(dir_exp)
+
+
 def main():
     logger.add('log.log')
+    logger.info('=== start ===')
     with open('config.yml', 'r') as yml:
         config = yaml.load(yml)
 
-    logger.info('=== start ===')
-    logger.info(f'{config["globals"]["dir_save"]}*')
+    # init
+    init_exp(config)
 
+    # get features
+    X, X_te = util.get_features(config)
 
 
 if __name__ == "__main__":
